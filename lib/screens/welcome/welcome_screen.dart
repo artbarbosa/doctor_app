e0fa3a46479d60ce8b5d58b83528699b970cf4d6
artbarbosa <arthur.bos@hotmail.com>
2021-11-09 20:48:51 -0300

    Initial commit

---
+++ lib/screens/welcome/welcome_screen.dart
@@ -0,0 +1,91 @@
+import 'package:doctor_app/constants.dart';
+import 'package:doctor_app/screens/auth/sign_in_screen.dart';
+import 'package:doctor_app/screens/auth/sign_up_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'dart:ui' as ui;
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          SvgPicture.asset("assets/icons/splash_bg.svg"),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+              child: Column(
+                children: [
+                  Spacer(),
+                
+                  Text(
+                    'Doctor App', 
+                      style: TextStyle(
+                        
+                        fontWeight: FontWeight.bold,
+                        fontSize: 60,
+                        foreground: Paint()
+                          ..shader = ui.Gradient.linear(
+                            const Offset(0, 150),
+                            const Offset(90, 80),
+                            <Color>[
+                              Colors.white, 
+                              Colors.tealAccent,
+                            ],
+                          )
+                      ),
+                  ),
+                  Spacer(),
+                  // As you can see we need more paddind on our btn
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignUpScreen(),
+                        ),
+                      ),
+                      style: TextButton.styleFrom(
+                        backgroundColor: Color(0xFF6CD8D1),
+                      ),
+                      child: Text("Sign Up"),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: defaultPadding),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SignInScreen(),
+                            )),
+                        style: TextButton.styleFrom(
+                          // backgroundColor: Color(0xFF6CD8D1),
+                          elevation: 0,
+                          backgroundColor: Colors.transparent,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(color: Color(0xFF6CD8D1)),
+                          ),
+                        ),
+                        child: Text("Sign In"),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: defaultPadding),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
